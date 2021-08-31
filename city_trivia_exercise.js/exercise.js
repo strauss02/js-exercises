459c645e348be77b9c2ccf6e6724ccd87e69b3f8
Ido Strauss <strauss02@hotmail.co.il>
2021-08-31 20:12:44 +0300

    Update exercise.js
    works

--- city_trivia_exercise.js/exercise.js
+++ city_trivia_exercise.js/exercise.js
@@ -3,56 +3,63 @@ const cities = [
         isCapital:true,
         isCoastal:false,
         isFamous:true,
-        isAncient:true 
+        isAncient:true ,
+        cityName: "Jerusalem"
     },
     Tel_Aviv = {
         isCapital:false,
         isCoastal:true,
         isFamous:true,
-        isAncient:false 
+        isAncient:false ,
+        cityName: "Tel Aviv"
     },
     Acre  = {
         isCapital:false,
         isCoastal:true,
         isFamous:false,
-        isAncient:true 
+        isAncient:true ,
+        cityName: "Acre"
     },
     Katzrin  = {
         isCapital:false,
         isCoastal: false,
         isFamous: false,
-        isAncient:true 
+        isAncient:true ,
+        cityName: "Katzrin"
     },
     Zikim  = {
         isCapital: false,
         isCoastal: true,
         isFamous: false,
-        isAncient:false 
+        isAncient:false ,
+        cityName: "Zikim"
     },
     Musmus  = {
         isCapital: false,
         isCoastal: false,
         isFamous: false,
-        isAncient: false 
+        isAncient: false ,
+        cityName: "Musmus"
     }
 ]
 
+
 function guessCity(capital, coastal, famous, ancient) {
 
-    let resultArray = cities.map(checkIfPropsMatch)
+    let triviaAnswer = "No city matches those properties."
 
     function checkIfPropsMatch (object) {
-        if (capital === object[isCapital] && coastal === object[isCoastal] && famous === object[isFamous] && ancient === object[isAncient]  ) {
-            return true
+        if (capital === object.isCapital && coastal === object.isCoastal && famous === object.isFamous && ancient === object.isAncient  ) {
+            triviaAnswer = object.cityName
         }
-            else {
-                return false
-            }
-    
-}
+
+    }
+    cities.map(checkIfPropsMatch)
+
+    return triviaAnswer
 
 }
 
-console.log(cities.length)
+guessCity(false,false,false,false)
 
 
