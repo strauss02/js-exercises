7e378f845f63a80ab521fa53b383b629fdbb5633
Ido Strauss <strauss02@hotmail.co.il>
2021-08-31 19:48:48 +0300

    Update exercise.js

--- city_trivia_exercise.js/exercise.js
+++ city_trivia_exercise.js/exercise.js
@@ -1,46 +1,58 @@
+const cities = [
+    Jerusalem = {
+        isCapital:true,
+        isCoastal:false,
+        isFamous:true,
+        isAncient:true 
+    },
+    Tel_Aviv = {
+        isCapital:false,
+        isCoastal:true,
+        isFamous:true,
+        isAncient:false 
+    },
+    Acre  = {
+        isCapital:false,
+        isCoastal:true,
+        isFamous:false,
+        isAncient:true 
+    },
+    Katzrin  = {
+        isCapital:false,
+        isCoastal: false,
+        isFamous: false,
+        isAncient:true 
+    },
+    Zikim  = {
+        isCapital: false,
+        isCoastal: true,
+        isFamous: false,
+        isAncient:false 
+    },
+    Musmus  = {
+        isCapital: false,
+        isCoastal: false,
+        isFamous: false,
+        isAncient: false 
+    }
+]
+
 function guessCity(capital, coastal, famous, ancient) {
-    const cities = [
-        Jerusalem = {
-            isCapital:true,
-            isCoastal:false,
-            isFamous:true,
-            isAncient:true 
-        },
-        Tel_Aviv = {
-            isCapital:false,
-            isCoastal:true,
-            isFamous:true,
-            isAncient:false 
-        },
-        Acre  = {
-            isCapital:false,
-            isCoastal:true,
-            isFamous:false,
-            isAncient:true 
-        },
-        Katzrin  = {
-            isCapital:false,
-            isCoastal: false,
-            isFamous: false,
-            isAncient:true 
-        },
-        Zikim  = {
-            isCapital: false,
-            isCoastal: true,
-            isFamous: false,
-            isAncient:false 
-        },
-        Musmus  = {
-            isCapital: false,
-            isCoastal: false,
-            isFamous: false,
-            isAncient: false 
+
+    let resultArray = cities.map(checkIfPropsMatch)
+
+    function checkIfPropsMatch (object) {
+        if (capital === object[isCapital] && coastal === object[isCoastal] && famous === object[isFamous] && ancient === object[isAncient]  ) {
+            return true
         }
-
-
-    ]
-
+            else {
+                return false
+            }
     
-
+}
 
 }
+
+console.log(cities.length)
+
+
