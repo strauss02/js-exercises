a937d8f99a9142d804b8294ed164488eabeb8925
Ido Strauss <strauss02@hotmail.co.il>
2021-08-31 19:15:51 +0300

    add cities as array of objects

---
+++ city_trivia_exercise.js/exercise.js
@@ -0,0 +1,46 @@
+function guessCity(capital, coastal, famous, ancient) {
+    const cities = [
+        Jerusalem = {
+            isCapital:true,
+            isCoastal:false,
+            isFamous:true,
+            isAncient:true 
+        },
+        Tel_Aviv = {
+            isCapital:false,
+            isCoastal:true,
+            isFamous:true,
+            isAncient:false 
+        },
+        Acre  = {
+            isCapital:false,
+            isCoastal:true,
+            isFamous:false,
+            isAncient:true 
+        },
+        Katzrin  = {
+            isCapital:false,
+            isCoastal: false,
+            isFamous: false,
+            isAncient:true 
+        },
+        Zikim  = {
+            isCapital: false,
+            isCoastal: true,
+            isFamous: false,
+            isAncient:false 
+        },
+        Musmus  = {
+            isCapital: false,
+            isCoastal: false,
+            isFamous: false,
+            isAncient: false 
+        }
+
+
+    ]
+
+    
+
+
+}
